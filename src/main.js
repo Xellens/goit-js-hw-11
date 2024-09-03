ee92d3831468d385a656acfec6cd6c00d43171cd
Xelle <oksana2854x@gmail.com>
2024-09-03 16:54:59 +0200

    fix

--- src/main.js
+++ src/main.js
@@ -10,32 +10,41 @@ const searchFormEl = document.querySelector('.js-search-form');
 const galleryEL = document.querySelector('.js-gallery');
 const loaderEl = document.querySelector('.js-loader');
 
+const lightbox = new SimpleLightbox('.js-gallery a', {
+  overlay: true,
+  captionsData: 'alt',
+  overlayOpacity: 0.8,
+  captionDelay: 250,
+  focus: true,
+});
+
 function showLoader() {
   loaderEl.classList.remove('is-hidden');
 }
+
 function hideLoader() {
   loaderEl.classList.add('is-hidden');
 }
-
-showLoader();
-setTimeout(hideLoader, 2000);
 
 const onSearchFormSubmit = event => {
   event.preventDefault();
   const searchedValue = searchFormEl.elements.user_query.value.trim();
 
+  if (searchedValue === '') {
+    iziToast.warning({
+      title: 'Caution',
+      message: 'Input field must not be empty',
+      position: 'topLeft',
+    });
+    galleryEL.innerHTML = '';
+    searchFormEl.reset();
+    return;
+  }
+
+  showLoader();
+
   fetchPhotos(searchedValue)
     .then(data => {
-      if (searchedValue === '') {
-        iziToast.warning({
-          title: 'Caution',
-          message: 'Input field must not be empty',
-          position: 'topLeft',
-        });
-        galleryEL.innerHTML = '';
-        searchFormEl.reset();
-        return;
-      }
       if (data.hits.length === 0) {
         iziToast.error({
           message:
@@ -46,23 +55,25 @@ const onSearchFormSubmit = event => {
         searchFormEl.reset();
         return;
       }
-      console.log(data);
+
       const galleryCards = data.hits
         .map(imgDetals => createGalleryCard(imgDetals))
         .join('');
       galleryEL.innerHTML = galleryCards;
 
-      const lightbox = new SimpleLightbox('.js-gallery a', {
-        overlay: true,
-        captionsData: 'alt',
-        overlayOpacity: 0.8,
-        captionDelay: 250,
-        focus: true,
-      });
       lightbox.refresh();
     })
     .catch(err => {
+      iziToast.error({
+        title: 'Error',
+        message: `Something went wrong: ${err.message}`,
+        position: 'topRight',
+      });
       console.log(err);
+    })
+    .finally(() => {
+      hideLoader();
     });
 };
+
 searchFormEl.addEventListener('submit', onSearchFormSubmit);
